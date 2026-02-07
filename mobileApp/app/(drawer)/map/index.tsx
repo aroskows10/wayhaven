10458ea2e9f227274e4f9f986b283a061fdf61d2
aroskows10 <aroskowski10@gmail.com>
2026-02-06 22:19:00 -0800

    we have a map! (using mapbook)

--- mobileApp/app/(drawer)/map/index.tsx
+++ mobileApp/app/(drawer)/map/index.tsx
@@ -1,24 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Mapbox from "@rnmapbox/maps";
 
-
+Mapbox.setAccessToken("pk.eyJ1IjoiYXJvc2tvd3NraSIsImEiOiJjbWxidGdxaWMwbDBpM2RxMmE4cWZwemszIn0.-_B2ws8d6EHQmntsInt5FQ");
 
 export default function Map() {
   return (
-    <>
-      <View style={styles.container}>
-       
-        <Text>This is the index page of MAP (default Home)r</Text>
-      </View>
-     
-    </>
+    <View style={styles.container}>
+      <Mapbox.MapView style={styles.map}>
+        <Mapbox.Camera
+          zoomLevel={14}
+          followUserLocation={true}
+        />
+        <Mapbox.UserLocation visible={true} />
+      </Mapbox.MapView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#16a355ff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  map: {
+    flex: 1,
   },
 });
